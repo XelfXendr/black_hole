dd34f16191bc1e965138d6860322df83604fd644
Jan Bronec <49397215+XelfXendr@users.noreply.github.com>
2020-04-21 16:53:50 +0200

    3D view
    More efficient rays
    3D orthogonal view
    Multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,97 @@
 use nalgebra::{Rotation3, Vector3, Unit};
 use image::{RgbImage, Rgb};
-use std::cmp::{max, min};
-use std::f64::consts::PI;
+use std::time::Instant;
+use std::thread;
+use std::collections::VecDeque;
 
 const C: f64 = 299792458.0;
 const M: f64 = 8.3e36;
 const G: f64 = 6.6743e-11;
+const RS: f64 = 2f64 / 3f64 * G * M / C / C; //it should be 3 times larger...
+const SCALE: f64 = 20f64 / RS;
+
+/*
+TODO:
+    Perspective camera
+    Improve multithreading (don't spawn gazillion threads at the same time)
+    Forget scale
+    Ask for user input (resolution, sampling, position, FOV)
+    Use better colors (and textures?) for ring and skybox
+*/
 
 fn main() {
-    let rs = 2f64 / 3f64 * G * M / C / C /*wtf?*/;
-    let scale = 100f64 / rs;
-    let pos_blackhole: Vector3<f64> = Vector3::new(500f64,500f64,0f64) / scale;
-
-    let mut img = image::RgbImage::new(1000, 1000);
-
-    let y = 174f64;
-    let spacing = 0.1f64;
-
-    for i in 0..100
+    let pos_blackhole: Vector3<f64> = Vector3::new(100f64,100f64,100f64) / SCALE;
+    let mut img = RgbImage::new(200, 200);
+    let mut q: VecDeque<thread::JoinHandle<(u32, u32, Rgb<u8>)>> = VecDeque::new();
+    
+    let now = Instant::now();
+    for j in 0..200
     {
-        send_ray(&mut img, &pos_blackhole, scale, Vector3::new(1000f64, y + i as f64 * spacing, 0f64)/scale, Vector3::new(-1f64, 0f64, 0f64));
+        for i in 0..200
+        {
+            let i_clone = i;
+            let j_clone = j;
+            let pos_blackhole_clone = pos_blackhole;
+            let t = thread::spawn(move || {
+                return (i_clone, j_clone, send_ray(pos_blackhole_clone, Vector3::new(i_clone as f64, j_clone as f64 - 30f64, -50f64)/SCALE, Vector3::new(0f64, 0.2f64, 1f64))); 
+            });
+
+            q.push_back(t);
+        }
     }
 
-    draw_circle(&mut img, (pos_blackhole.x * scale) as u32, (pos_blackhole.y * scale) as u32, (rs * scale) as u32, Rgb([0, 0, 255]));
-
+    for r in q
+    {
+        let (i, j, rgb) = r.join().unwrap();
+        img.put_pixel(i, j, rgb);
+    }
+    println!("{:?}", now.elapsed());
+    
     img.save("image.png").unwrap();
 }
 
-fn send_ray(img: &mut RgbImage, pos_blackhole: &Vector3<f64>, scale: f64, mut pos_photon: Vector3<f64>, mut dir_photon: Vector3<f64>)
+fn send_ray(pos_blackhole: Vector3<f64>, mut pos_photon: Vector3<f64>, mut dir_photon: Vector3<f64>) -> Rgb<u8>
 {
     dir_photon = *Unit::new_normalize(dir_photon) * C;
-
-    let delta_time = 0.01f64;
-
-    for _i in 0..100000
+    let mut delta_time: f64;
+    loop
     {
+        let dist = (pos_blackhole - pos_photon).magnitude();
+        if dist < RS 
+        {
+            return Rgb([0,0,0]);
+        }
+        if dist > RS*10f64
+        {
+            return Rgb([0,0,50]);
+        }
+
+        if dist < 2f64*RS
+        {
+            delta_time = 0.001f64;
+        }
+        else
+        {
+            delta_time = dist / RS - 2f64;
+            delta_time = 0.999 / 4096.0 * delta_time * delta_time * delta_time * delta_time + 0.001;
+        }
+
         let dist_vec = pos_blackhole - pos_photon;
         let dist: f64 = dist_vec.magnitude();
         let force = G * M / dist / dist;
-        let delta_theta = (PI - dist_vec.angle(&dir_photon)).sin() * force / C * delta_time;
+        let delta_theta = (dist_vec.angle(&dir_photon)).sin() * force / C * delta_time;
         let u = dir_photon.cross(&dist_vec);
-
-        let rotation_matrix = Rotation3::from_axis_angle(&Unit::new_normalize(u), delta_theta);
-        dir_photon = rotation_matrix * dir_photon;
-
+        if u.magnitude() != 0f64
+        {
+            let rotation_matrix = Rotation3::from_axis_angle(&Unit::new_normalize(u), delta_theta);
+            dir_photon = rotation_matrix * dir_photon;
+        }
         let new_pos = pos_photon + dir_photon * delta_time;
 
-        if !(pos_photon.x < 0.0 || pos_photon.y < 0.0 || new_pos.x < 0.0 || new_pos.y < 0.0)
+        if (((new_pos.y - pos_blackhole.y) < 0f64) ^ ((pos_photon.y - pos_blackhole.y) < 0f64)) && dist > 3f64*RS && dist < 6f64*RS
         {
-            draw_line(img, (pos_photon.x * scale) as u32, (pos_photon.y * scale) as u32, (new_pos.x * scale) as u32, (new_pos.y * scale) as u32, Rgb([255, 255, 0]));
+            return Rgb([255, 128, 0]);
         }
         pos_photon = new_pos;
-    }
-}
-
-fn draw_circle(img: &mut RgbImage, x: u32, y: u32, r: u32, rgb: Rgb<u8>) {
-    for i in max(0, x as i32 - r as i32) as u32 .. min(img.width(), x + r + 1)
-    {
-        for j in max(0, y as i32 - r as i32) as u32 .. min(img.height(), y + r + 1)
-        {
-            if (((i as i32 - x as i32).pow(2) + (j as i32 - y as i32).pow(2)) as f32).sqrt() <= r as f32
-            {
-                img.put_pixel(i, j, rgb);
-            }
-        }
-    }
-}
-
-fn draw_line(img: &mut RgbImage, x1: u32, y1: u32, x2: u32, y2: u32, rgb: Rgb<u8>)
-{
-    let l = (x2 as f64 - x1 as f64 + y2 as f64 - y1 as f64).abs();
-
-    for i in 0..(l as u32 + 1)
-    {
-        let x = ((x1 as f64 * i as f64 + x2 as f64 * (l - i as f64)) / l) as u32;
-        let y = ((y1 as f64 * i as f64 + y2 as f64 * (l - i as f64)) / l) as u32;
-        draw_circle(img, x, y, 1, rgb);
-        //img.put_pixel((x1 * i + x2 * (l - i)) / l, (y1 * i + y2 * (l - i)) / l, rgb);
     }
 }
